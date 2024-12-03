518984c1ad41c087ffdb3e167e18c0d030124935
Reza Saputra <129853013+rezasaputra4250@users.noreply.github.com>
2024-12-03 10:58:06 +0700

    Create MultiplyPage.js

---
+++ src/pages/MultiplyPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const MultiplyPage = () => {
+  return (
+    <div>
+      <h2>Multiplication Page</h2>
+      <p>Perform multiplication operations here.</p>
+    </div>
+  );
+};
+
+export default MultiplyPage;
