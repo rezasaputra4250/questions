9d392f22ece746e523e6e2db86a09d0fc36cb55a
Reza Saputra <129853013+rezasaputra4250@users.noreply.github.com>
2024-12-03 10:58:08 +0700

    Create SubtractPage.js

---
+++ src/pages/SubtractPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const SubtractPage = () => {
+  return (
+    <div>
+      <h2>Subtraction Page</h2>
+      <p>Perform subtraction operations here.</p>
+    </div>
+  );
+};
+
+export default SubtractPage;
