8b517a6bdfaf1d37305297faa96e42dae73c4ddf
Reza Saputra <129853013+rezasaputra4250@users.noreply.github.com>
2024-12-03 10:58:02 +0700

    Create DividePage.js

---
+++ src/pages/DividePage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const DividePage = () => {
+  return (
+    <div>
+      <h2>Division Page</h2>
+      <p>Perform division operations here.</p>
+    </div>
+  );
+};
+
+export default DividePage;
